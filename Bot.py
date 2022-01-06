0d0246e835628d9542901cbee633194bdf9873c0
TPBubbleU <66184375+TPBubbleU@users.noreply.github.com>
2022-01-06 00:44:36 -0800

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -89,14 +89,13 @@ async def on_voice_state_update(member, before, after):
       await before.channel.delete()
       
   # Lets have a random chance to make a sound on entry
-  if random.random() <= .1 and after.channel.name != SpecialChannelName: #10% chance
+  if random.random() <= 1 and after.channel.name != SpecialChannelName: #10% chance
     print(f"Started the process of making random noise at {datetime.datetime.now()}")
     for i in bot.voice_clients:
       await i.disconnect(force=True)
     vc = await after.channel.connect()
-    vc.play(discord.FFmpegPCMAudio('TheGoodtheBadandtheUgly.mp3'), after=None)
-    vc.source.volume -= 0.2
-    time.sleep(7)
+    vc.play(discord.FFmpegPCMAudio('TheGoodtheBadandtheUgly.mp3'),options='-filter:a "volume=4.0"')
+    time.sleep(5)
     await vc.disconnect(force=True)
   print(f"Ended a on_voice_state_update at {datetime.datetime.now()}")
 
