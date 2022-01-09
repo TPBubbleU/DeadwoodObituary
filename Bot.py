c0e7bc9ccf24c82ccc35e187813b753d7bc5be3c
TPBubbleU <66184375+TPBubbleU@users.noreply.github.com>
2022-01-08 19:55:05 -0800

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -13,7 +13,7 @@ UsersLists = {}
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = discord.Bot(command_prefix="!", intents=intents)
 
 @bot.slash_command(guild_ids=[882043693274628167])
 async def ShowMyList(ctx):
