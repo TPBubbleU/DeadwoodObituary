20f3256555d6a4f62e7f177b309bac98482c73c4
TPBubbleU <66184375+TPBubbleU@users.noreply.github.com>
2022-02-06 22:32:17 -0800

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -44,7 +44,7 @@ async def bigtest(ctx, text: discord.Option(str, required=True)):
   print(f"Started a test command at {datetime.datetime.now()} ")
   interaction = await ctx.respond(content=f"Saying '{text}' ", ephemeral=True)
   time.sleep(5)
-  await interaction.response.edit_original_message(content="We changed the thing")
+  await interaction.edit_original_message(content="We changed the thing")
   
 @bot.slash_command(guild_ids=servers, name="the-hands-voice", description="Make the hand say something in a voice")
 async def renameposse(ctx, channel: discord.Option(discord.VoiceChannel, required=True)
