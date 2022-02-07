4b0904b3e64ff79c68a4107479d8ade501b8b4d1
TPBubbleU <66184375+TPBubbleU@users.noreply.github.com>
2022-02-06 22:20:34 -0800

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -43,8 +43,9 @@ servers = [882043693274628167]
 async def renameposse(ctx, text: discord.Option(str, required=True)):
   print(f"Started a test command at {datetime.datetime.now()} ")
   interaction = await ctx.respond(content=f"Saying '{text}' ", ephemeral=True)
+  followup = await interaction.followup.send(content=f"Saying '{text}' ", ephemeral=True)
   time.sleep(5)
-  await interaction.response.edit_message(content="We changed the thing")
+  await followup.response.edit_message(content="We changed the thing")
   
 @bot.slash_command(guild_ids=servers, name="the-hands-voice", description="Make the hand say something in a voice")
 async def renameposse(ctx, channel: discord.Option(discord.VoiceChannel, required=True)
