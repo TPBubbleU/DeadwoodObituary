09c64ab9ce4878477d47dcf6fa677cb8380632f3
TPBubbleU <66184375+TPBubbleU@users.noreply.github.com>
2022-02-13 15:09:57 -0800

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -224,10 +224,10 @@ async def spotify(ctx):
     await interaction.response.send_modal(modal)
   
   linkview = View()
-  modalSpawningButton = Button(label="Click Here", callback=modal_for_button_click)
+  modalSpawningButton = Button(label="Click Here for input")
+  modalSpawningButton.callback = modal_for_button_click
   
-  
-  linkview.add_item(Button(label="Click Here", url=link))
+  linkview.add_item(Button(label="Click Here for link", url=link))
   linkview.add_item(modalSpawningButton)
   await ctx.respond(content=f"Lets start by getting you setup \nYou can use this link to get a new auth token", ephemeral=True, view=linkview)
   
