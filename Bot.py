f02c59ed2a3b13252e79f60a8b5f7b6204859c76
TPBubbleU <66184375+TPBubbleU@users.noreply.github.com>
2022-02-13 19:19:59 -0800

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -272,7 +272,7 @@ async def spotify(ctx):
       # Save our access token into our user list object
       UsersLists[ctx.author.id]['SpotifyAccess'] = auth['access_token']
       content = f"{ctx.author} has decided to live dangerously and give control of his spotify to chat "
-      await ctx.interaction.edit_original_message(content=content, view=command_view, embeds=[get_current_song_embed()])
+      await interaction.response.edit_message(content=content, view=command_view, embeds=[get_current_song_embed()])
       
     modal.callback = callback_for_modal
     await interaction.response.send_modal(modal)
