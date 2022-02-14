4a4adc37e413aad25ef8583e708314f77709f850
TPBubbleU <66184375+TPBubbleU@users.noreply.github.com>
2022-02-13 19:42:35 -0800

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -260,7 +260,7 @@ async def spotify(ctx):
     modal = Modal(title="Lets get that input baby!")
     modal.add_item(InputText(label="Enter key here: ", value= 'Get this from the link'))
     async def callback_for_modal(interaction):
-      await interaction.response.pong()
+      await interaction.response.send_message()
       # Setup things to get the access token from Spotifys API
       body = {
         'client_id':clientId,
